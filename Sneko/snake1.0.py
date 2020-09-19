cd2cc3abd96851c4de054a17f92064f3e52bdb9a
fellipeconeg <71567600+fellipeconeg@users.noreply.github.com>
2020-09-19 20:35:00 +0100

    Add files via upload

---
+++ Sneko/snake1.0.py
@@ -0,0 +1,133 @@
+import pygame, random
+import time
+from pygame.locals import *
+
+def on_grid_random():
+    x = random.randint(0, 59)
+    y = random. randint(0, 59)
+    return (x * 10, y * 10)
+
+def collision(c1, c2):
+    return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        gameDisplay.fill(white)
+        largeText = pygame.font.Font('freesansbold.ttf',115)
+        TextSurf, TextRect = text_objects("A bit Racey", largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
+        clock.tick(15)
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+pygame.display.set_caption('Snake')
+
+snake = [(200,200), (210,200), (220,200)]
+snake_skin = pygame.Surface((10,10))
+snake_skin.fill((255,255,255))
+
+apple_pos = on_grid_random()
+apple = pygame.Surface((10,10))
+apple.fill((255, 0, 0))
+
+my_direction = LEFT
+
+clock = pygame.time.Clock()
+
+font = pygame.font.Font('freesansbold.ttf', 18)
+score = 0
+
+
+
+game_over = False
+while not game_over:
+    clock.tick(10)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+        if event.type == KEYDOWN:
+            if event.key == K_UP  and my_direction != DOWN:
+                my_direction = UP
+            if event.key == K_DOWN and my_direction != UP:
+                my_direction = DOWN
+            if event.key == K_LEFT and my_direction != RIGHT:
+                my_direction = LEFT
+            if event.key == K_RIGHT and my_direction != LEFT:
+                my_direction = RIGHT
+
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
+        score = score + 1
+    
+    #checar se a cobra colidiu com as paredes
+    if snake[0][0] == 600 or snake[0][1] == 600 or snake[0][0] < 0 or snake[0][1] < 0:
+        game_over = True
+        break
+    
+    #checar se a snake colidiu com si própria
+    for i in range(1, len(snake) -1):
+        if snake[0][0] == snake[i][0] and snake[0][1] == snake[i][1]:
+            game_over = True
+            break
+
+    if game_over:
+        break
+
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+    
+    if my_direction == UP:
+        snake[0] = (snake[0][0], snake[0][1] - 10)
+    if my_direction == DOWN:
+        snake[0] = (snake[0][0], snake[0][1] + 10)
+    if my_direction == RIGHT:
+        snake[0] = (snake[0][0] + 10, snake[0][1])
+    if my_direction == LEFT:
+        snake[0] = (snake[0][0] - 10, snake[0][1])
+
+    screen.fill((0, 0, 0))
+    screen.blit(apple, apple_pos)
+
+    score_font = font.render('Score: %s' % (score), True, (255, 255, 255))
+    score_rect = score_font.get_rect()
+    score_rect.topleft = (600 - 120, 10)
+    screen.blit(score_font, score_rect)
+
+    for pos in snake:
+        screen.blit(snake_skin, pos)
+
+    pygame.display.update()
+
+while True:
+    game_over_font = pygame.font.Font('freesansbold.ttf', 75)
+    game_over_screen = game_over_font.render('RUIM DEMAIS', True, (255, 255, 255))
+    game_over_rect = game_over_screen.get_rect()
+    game_over_rect.midtop = (600 / 2, 10)
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.update()
+    pygame.time.wait(500)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+    
